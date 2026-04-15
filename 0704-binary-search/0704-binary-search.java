9726c64a2c0913cdcc9a5535c68625611f08fcc2
Kartik Tyagi <tyagikartik735@gmail.com>
2026-04-15 22:10:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,8 +2,8 @@ class Solution {
     public int search(int[] nums, int target) {
         int low =0;
         int high = nums.length-1;
-       if (low==high && target==nums[low]) return low;
-        while(low<high)
+      
+        while(low<=high)
         {
              int mid =( low + high )/2;
 if(nums[low]==target){
