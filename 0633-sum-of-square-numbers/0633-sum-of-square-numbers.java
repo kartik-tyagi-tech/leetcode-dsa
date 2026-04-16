1e2147e85a1c4c4465d6c24c93b9d739a6bcc70f
Kartik Tyagi <tyagikartik735@gmail.com>
2026-04-16 15:35:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean judgeSquareSum(int c) {
         int low =0;
         int high =(int)Math.sqrt(c);
-        while(low<=high){
+        while(low<high){
             long sum=(long)low*low+(long)high*high;
             if(sum==c){
                    return true;
