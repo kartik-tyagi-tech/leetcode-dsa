b9dd19fee7d81bb7608d59d58afd596bab2a04f8
Kartik Tyagi <tyagikartik735@gmail.com>
2026-04-16 15:34:20 +0530

    Time: 4 ms (83.27%), Space: 41.8 MB (96.16%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        // Use long to prevent overflow during intermediate calculations (a*a + b*b)
-        long left = 0;
-        long right = (long) Math.sqrt(c);
-        
-        while (left <= right) {
-            long sum = left * left + right * right;
-            if (sum == c) {
-                return true;
-            } else if (sum < c) {
-                left++;
-            } else {
-                right--;
+        int low =0;
+        int high =(int)Math.sqrt(c);
+        while(low<=high){
+            long sum=(long)low*low+(long)high*high;
+            if(sum==c){
+                   return true;
+            }
+            else if(sum>c){
+                high--;
+            }
+            else {
+                low++;
             }
         }
         return false;
