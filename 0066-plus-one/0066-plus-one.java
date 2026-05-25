21942b955de2163416a0e2e665b2548af3231810
Kartik Tyagi <tyagikartik735@gmail.com>
2026-05-25 20:19:58 +0530

    Time: 0 ms (100%), Space: 43.6 MB (8.59%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,14 @@
-import java.math.BigInteger;
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        
-        String s="";
-        for(int each:digits){
-            s+=each;
+        for(int i =digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
         }
-        BigInteger num=new BigInteger(s);
-        num=num.add(new BigInteger("1"));
-        String s1=num.toString();
-        int nums[]=new int[s1.length()];
-        for(int i =0;i<s1.length();i++){
-            nums[i]=s1.charAt(i)-'0';
-        }
-        return nums;
+        int d[]=new int[digits.length+1];
+        d[0]=1;
+        return d;
     }
 }
