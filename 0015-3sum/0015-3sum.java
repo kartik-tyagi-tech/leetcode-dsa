0964c8da20890884338839bf4fbca858bb565293
Kartik Tyagi <tyagikartik735@gmail.com>
2026-07-26 23:40:06 +0530

    Time: 1339 ms (5.72%), Space: 56.6 MB (95.09%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-             
-             int i=0;
-        while(i < nums.length-2) {
-            
-            int j = i + 1;
-            int k = nums.length - 1;
-            int temp1=nums[i];
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    temp1=nums[i];
-                    int temp2 = nums[j];
-                    int temp3 = nums[k];
+        int n = nums.length;
 
-                    while (j <= nums.length - 1 && k >= 0 && nums[j] == temp2 && nums[k] == temp3) {
-                        k--;
-                        j++;
-                    }
-                } else if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                }
+        HashSet<Integer>set=new HashSet<>();
+        HashSet<List<Integer>> check = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
 
+        for(int i =0;i<n;i++){
+            int j = i+1;
+            while(j<n){
+                int needed = -(nums[i]+nums[j]);
+                         if(set.contains(needed)) {
+                ArrayList<Integer> list = new ArrayList<>();
+                   list.add(nums[i]);
+                list.add(nums[j]);
+                  list.add(needed);
+                Collections.sort(list);
+                check.add(list);
+                j++;
+            }else {
+                set.add(nums[j]);
+                j++;
             }
-            
-            while(i < nums.length-2 && nums[i]==temp1){
-                  i++;
-            }
-            
-            
+             } 
+             set.clear();
         }
-
-        return list;
+        for(List each:check){
+            result.add(each);
+        }
+        return result;
     }
 }
